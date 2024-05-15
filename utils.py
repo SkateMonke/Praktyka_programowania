b8905574bfb14847fc03811765f1939a77e615af
SkateMonke <bartlomiej.gosciniak7@gmail.com>
2024-05-15 14:23:25 +0200

    Poprawiono docstringi

--- utils.py
+++ utils.py
@@ -1,19 +1,21 @@
-""""""
+"""
+GONGAGA
+"""
 def add(a: int, b: int) -> int:
-    """"""
+    """FUNKCJA DODAWANIA"""
     return a + b
 
 
 def subtract(a: int, b: int) -> int:
-    """"""
+    """FUNKCJA ODEJMOWANIA"""
     return a - b
 
 
 def multiply(a: int, b: int) -> int:
-    """"""
+    """FUNKCJA MNOZENIA"""
     return a * b
 
 
 def divide(a: int, b: int) -> float:
-    """"""
+    """FUNKCJA DZIELENIA"""
     return a / b
